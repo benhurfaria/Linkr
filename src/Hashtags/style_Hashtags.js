96bbd37dfabe0f9d290ed724273b93d0f13c488a
João <joaotamagno@gmail.com>
2021-09-16 14:24:50 -0300

    Show hastags

--- src/Hashtags/style_Hashtags.js
+++ src/Hashtags/style_Hashtags.js
@@ -36,6 +36,7 @@ const Trending = styled.div`
     width: 230px;
     background-color: #151515;
     border-radius: 10px;
+    margin: 10px 0 0 10px;
 `
 
 export { Sharp, SharpList, TrendingTitle, Border, Trending }
