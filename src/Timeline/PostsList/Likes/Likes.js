2b6fd52afe203d2b6742c7032e2a24066021653b
João <joaotamagno@gmail.com>
2021-09-20 11:22:24 -0300

    Likes

--- src/Timeline/PostsList/Likes/Likes.js
+++ src/Timeline/PostsList/Likes/Likes.js
@@ -1,32 +1,23 @@
-import { useEffect,  useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsHeart, BsHeartFill } from 'react-icons/bs'
 import styled from 'styled-components';
 import ReactTooltip from 'react-tooltip'
 import { useContext } from 'react';
 import { LoggedUser } from '../../../services/contexts/LoggedUser';
 import { giveLike, dislike } from '../../../services/api/Api';
-//import { LikeObj, Liked,NotLiked, Button, Counter} from './style_Likes'
+import { LikeObj, Liked, NotLiked, Button, Counter } from './style_Likes'
 
 export default function Likes({ likes, id }) {
-    
     const [liked, setLiked] = useState(false)
     const { loggedUser } = useContext(LoggedUser);
     const [likesCounter, setLikesCounter] = useState(likes.length);
     const [likesTip, setLikesTip] = useState([])
     useEffect(() => {
-       
-            likes.map(like => likesTip.push({ userId: like["user.id"], username: like["user.username"] }))
-        likesTip.forEach(like => {
-            if (like.id === loggedUser.id) {
-                setLiked(true)
-            }
-        })
-        
-        
-    }, [likes,  loggedUser])
+        likes.map(like => likesTip.push({ userId: like["user.id"], username: like["user.username"] }))
+        likesTip.forEach(like => {if(like.userId === loggedUser.id) setLiked(true)})
+    }, [likes, loggedUser, likesTip])
 
     function like() {
-
         const postId = id;
         const body = ''
         const config = {
@@ -34,8 +25,6 @@ export default function Likes({ likes, id }) {
                 Authorization: `Bearer ${loggedUser.token}`
             }
         };
-
-
         if (liked === false) {
             setLiked(true)
             const promise = giveLike(postId, config, body)
@@ -44,18 +33,17 @@ export default function Likes({ likes, id }) {
                 //console.log(likesTip)
                 setLikesCounter(res.data.post.likes.length)
             })
-            console.log(likesTip, id)
+
 
         } else {
             setLiked(false)
-            //setLikesCounter(likesCounter-1)
+
             const promise = dislike(postId, config, body)
             promise.then(res => {
                 setLikesTip(res.data.post.likes)
-                //console.log(likesTip)
                 setLikesCounter(res.data.post.likes.length)
             })
-            console.log(likesTip, id)
+
         }
     }
 
@@ -72,12 +60,23 @@ export default function Likes({ likes, id }) {
                     pessoa${likesTip.length === 3 ? '' : "s"} curti${likesTip.length === 3 ? 'u' : "ram"}`;
         }
     }
+
+    function showHeart() {
+        
+
+        if (liked) {
+            return <Liked ><BsHeartFill /> </Liked>
+        } else {
+            return <NotLiked ><BsHeart /></NotLiked>
+        }
+    }
     return (
         <>
 
             <LikeObj data-tip='' data-for={`${id}`}>
                 <Button key={id} type='button' onClick={like} >
-                    {liked ? <Liked ><BsHeartFill /> </Liked> : <NotLiked ><BsHeart /></NotLiked>}
+                    {showHeart()}
+                    {/* {liked===true ? <Liked ><BsHeartFill /> </Liked> : <NotLiked ><BsHeart /></NotLiked>} */}
                 </Button>
                 <Counter>{likesCounter} likes</Counter>
             </LikeObj>
@@ -90,30 +89,33 @@ export default function Likes({ likes, id }) {
 
 const StyledTooltip = styled(ReactTooltip)`
   font-family: 'Lato', sans-serif;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 200px;
 `
-const LikeObj = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #171717;
-`
-const NotLiked = styled.div`
-    font-size: 20px;
-    color: #ffffff; 
-`
-const Liked = styled.div`
-    color: #AC0000;
-    font-size: 20px;
-`
-const Button = styled.button`
-    margin: 0 10px 0 10px;
-    border: none;
-    background-color: #171717;
-    height: 30px;
-    width: 60px;
-`
-const Counter = styled.div`
-    color: #ffffff;
-    font-family: 'Lato', sans-serif;
-    font-size: 16px;
-`
+// const LikeObj = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     background-color: #171717;
+// `
+// const NotLiked = styled.div`
+//     font-size: 20px;
+//     color: #ffffff; 
+// `
+// const Liked = styled.div`
+//     color: #AC0000;
+//     font-size: 20px;
+// `
+// const Button = styled.button`
+//     margin: 0 10px 0 10px;
+//     border: none;
+//     background-color: #171717;
+//     height: 30px;
+//     width: 60px;
+// `
+// const Counter = styled.div`
+//     color: #ffffff;
+//     font-family: 'Lato', sans-serif;
+//     font-size: 16px;
+// `
