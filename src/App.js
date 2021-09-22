8a27330ed13f4ed1183678ffbffed9d6a43c3132
Tarcísio Teles <tfst@ecomp.poli.br>
2021-09-22 00:03:47 -0300

    finished route '/hashtag/:hashtag'

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default function App() {
                         </Route>
 
                         <Route exact path="/hashtag/:hashtagid">
-                            <HashtagPosts />
+                            <HashtagPosts  />
                         </Route>
                         
                     </Switch>
