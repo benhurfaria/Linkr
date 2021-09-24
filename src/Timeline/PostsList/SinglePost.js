84adbb297955ac94eb41fa39d4a26b4d64544e20
Tarcísio Teles <tfst@ecomp.poli.br>
2021-09-24 13:26:18 -0300

    patch: resolved text overflow for username, usertext and preview

--- src/Timeline/PostsList/SinglePost.js
+++ src/Timeline/PostsList/SinglePost.js
@@ -1,15 +1,14 @@
 import { Link, useHistory } from "react-router-dom";
 import { UserAvatar } from "../Timeline_style.js";
 import { PostLeftPanel } from "./NewPost/NewPost_style.js";
-import { Post, PostContent, PostPreview, PreviewInfo, ThumbPreview } from "./PostsList_style.js";
+import { Post, PostContent, PostPreview, PreviewInfo, ThumbPreview, UsernameLink, PostText } from "./SinglePost_style.js";
 
 import ReactHashtag from "react-hashtag";
-//import { IoIosHeart } from "react-icons/io";
 import Likes from "./Likes/Likes.js";
 
 export default function SinglePost({ post }) {
     const { id, likes, text, link, linkTitle, linkDescription, linkImage, user } = post;
-    
+
     const history = useHistory();
 
     function goToHashtag(hashtag) {
@@ -24,25 +23,26 @@ export default function SinglePost({ post }) {
                     <UserAvatar src={user.avatar} />
                 </Link>
                 <h1><Likes key={id} likes={likes} id={id} /></h1>
-                
+
             </PostLeftPanel>
             <PostContent>
-                <Link to={`/user/${user.id}`} >{user.username}</Link>
+                <UsernameLink><Link to={`/user/${user.id}`} >
 
-                <h2><ReactHashtag onHashtagClick={goToHashtag}>
+                    {user.username}
+                </Link></UsernameLink>
+
+                <PostText><ReactHashtag onHashtagClick={goToHashtag}>
                     {text}
-                </ReactHashtag></h2>
+                </ReactHashtag></PostText>
 
                 <PostPreview>
-                    <PreviewInfo>
-                        <a href={link} target="_blank" rel="noreferrer noopener">
-                            <h1> {linkTitle}</h1>
+                    <PreviewInfo href={link} target="_blank" rel="noreferrer noopener">
+                        <h1> {linkTitle}</h1>
 
-                            <h2>{linkDescription}</h2>
+                        <h2>{linkDescription}</h2>
 
-                            <h3>{link}</h3>
+                        <h3>{link}</h3>
 
-                        </a>
                     </PreviewInfo>
                     <ThumbPreview >
                         {
