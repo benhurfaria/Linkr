609f5d381f7c0733b95dddae9b0be7f4a0907cd0
João <joaotamagno@gmail.com>
2021-09-24 14:55:09 -0300

    teste

--- src/Timeline/Timeline.js
+++ src/Timeline/Timeline.js
@@ -1,79 +1,100 @@
-import React, { useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PostsList from "./PostsList/PostsList.js";
-import { MainContainer, ContainerHeader, ContainerPosts } from "./Timeline_style.js";
+import { MainContainer, ContainerHeader, ContainerPosts, LoaderPosition } from "./Timeline_style.js";
 import Hashtags from '../Hashtags/Hashtags'
-
+import InfiniteScroll from "react-infinite-scroller";
+import Loader from "react-loader-spinner";
 import Topbar from "../Topbar/Topbar.js";
 import { LoggedUser } from '../services/contexts/LoggedUser.js';
 import { ContextPost } from '../services/contexts/ContextPost.js';
-import { getAllPosts, getStoredUser, getUserPosts, getMyLikes } from "../services/api/Api.js";
+import { getAllPosts, getStoredUser, getUserPosts } from "../services/api/Api.js";
+
+import QueryString from "qs";
+
 
 export default function Timeline({ subType }) {
-    const { loggedUser} = useContext(LoggedUser);
+    const { loggedUser } = useContext(LoggedUser);
     const [postsArray, setPostsArray] = useState([]);
     const [postsLoaded, setPostsLoaded] = useState(false);
-    const [postTipo, setPostTipo] = useState(false);
-
+    const [loading, setLoading] = useState(true)
 
     function updatePostsArray(response) {
+        setLoading(true)
         if (response.data.posts.length < 1) {
-            setPostTipo(false);
+            alert("Nenhum post encontrado");
             return;
         }
         setPostsArray(response.data.posts);
-        setPostTipo(true);
         setPostsLoaded(true);
     };
-
-
     useEffect(() => {
-       
         const requestConfig = {
             headers: {
                 Authorization: `Bearer ${getStoredUser().token}`
             }
         };
         if (subType === "my posts") {
-
-            setPostsArray([]);
-            getUserPosts(requestConfig, loggedUser.id)
+            const params = QueryString.stringify({ limit: 10 })
+            setPostsArray([])
+            getUserPosts(requestConfig, loggedUser.id, params)
                 .then(updatePostsArray)
                 .catch(() => alert("Houve uma falha ao obter os posts, por favor atualize a página"));
         }
-
         if (subType === "timeline") {
-
-            setPostsArray([]);
-            getAllPosts(requestConfig)
+            const params = QueryString.stringify({ limit: 10 })
+            setPostsArray([])
+            getAllPosts(requestConfig, params)
+                .then(updatePostsArray)
+                .catch(() => alert("Houve uma falha ao obter os posts, por favor atualize a página"));
+        }
+    }, [loggedUser, subType,]);
+    function getMorePost(params) {
+        setLoading(false)
+        const strParams = QueryString.stringify(params)
+        const requestConfig = {
+            headers: {
+                Authorization: `Bearer ${getStoredUser().token}`
+            }
+        };
+        if (subType === "timeline") {
+            getAllPosts(requestConfig, strParams)
+                .then(updatePostsArray)
+                .catch(() => alert("Houve uma falha ao obter os posts, por favor atualize a página"));
+        }
+        if (subType === "my posts") {
+            getUserPosts(requestConfig, loggedUser.id, params)
                 .then(updatePostsArray)
                 .catch(() => alert("Houve uma falha ao obter os posts, por favor atualize a página"));
         }
 
-        if(subType === "my likes"){
-            setPostsArray([]);
-            getMyLikes(requestConfig)
-                .then(updatePostsArray)
-                .catch(() => alert("Houve uma falha ao obter os posts, por favor atualize a página"));
-        }
-
-    }, [loggedUser, subType]);
+    }
+    function loadingPost() {
+        return loading ? <></> : <LoaderPosition><Loader type="TailSpin" color="#00BFFF" height={80} width={80} /></LoaderPosition>
+    }
 
     return (
-
         <>
             <ContextPost.Provider value={{ postsArray, setPostsArray }}>
                 <Topbar />
-                <MainContainer>
+                <MainContainer style={{ height:"5000px ", overflowY: "auto"}} >
                     <ContainerHeader>
                         <h1>{`${subType}`}</h1>
                     </ContainerHeader>
-                    <ContainerPosts>
-                        <PostsList showList={postsLoaded} avatar={loggedUser.avatar} postsArray={postsArray} render={subType} postTipo={postTipo}/>
-                        <Hashtags />
-                    </ContainerPosts>
+                    <InfiniteScroll pageStart={0}
+                        loadMore={() => getMorePost({ limit: postsArray.length + 1 })}
+                        hasMore={true || false}
+                        loader={loadingPost()}
+                        useWindow={false}
+                        
+                        >
+                        <ContainerPosts>
+                            <PostsList showList={postsLoaded} avatar={loggedUser.avatar} postsArray={postsArray} render={subType} postTipo={postTipo}/>
+                            <Hashtags />
+                        </ContainerPosts>
+                    </InfiniteScroll>
                 </MainContainer>
             </ContextPost.Provider>
         </>
-
     );
 }
+
